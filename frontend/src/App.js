2d585ce9be3023e5d0ee1880b7184c186559882e
= <mayankrathore9897@gmail.com>
2024-08-12 13:31:50 +0530

    completing the profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import Body from "./component/Body";
-import { Home } from "./component/Home";
 
 function App() {
   return (
--- frontend/src/component/Home.js
+++ frontend/src/component/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Leftsidebar } from "./Leftsidebar";
 import { Rightsidebar } from "./Rightsidebar";
-import Feed from "./Feed";
+
 import { Outlet } from "react-router-dom";
 export const Home = () => {
   return (
