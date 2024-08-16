2600166dced04ddd46b30eb9810bf87d52c85243
= <mayankrathore9897@gmail.com>
2024-08-17 02:10:00 +0530

    making database connection

--- frontend/src/component/Login.js
+++ frontend/src/component/Login.js
@@ -1,7 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [islogin, setlogin] = useState(true);
+  const loginsignuphandler = () => {
+    setlogin(!islogin);
+  };
+  return (
+    <>
+      <div className=" w-screen h-screen flex items-center justify-center">
+        <div className=" flex items-center justify-evenly w-[80%]">
+          <div>
+            <img
+              className="ml-5"
+              width={"360px"}
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLN1uOyYGBL6AIkwIyOR2KH9sMYqmn0CsOVA&s"
+              alt="logo"
+            />
+          </div>
+          <div>
+            <div className="my-5">
+              <h1 className="text-6xl font-bold ">Happening Now</h1>
+            </div>
+
+            <h1 className="text-2xl font-bold mt-4 mb-2">
+              {islogin ? "Login" : "Signup"}
+            </h1>
+            <form className="flex flex-col w-[55%]">
+              {!islogin && (
+                <>
+                  <input
+                    type="text"
+                    placeholder=" Name"
+                    className="outline-blue-400 border border-gray-800  px-4 py-1 my-2 text-xl rounded-full font-semibold"
+                  />
+                </>
+              )}
+              <input
+                type="text"
+                placeholder=" Email"
+                className="outline-blue-400 border border-gray-800  px-4 py-1 my-2 text-xl rounded-full font-semibold"
+              />
+              <input
+                type="text"
+                placeholder=" Password"
+                className="outline-blue-400 border border-gray-800   px-4 py-1 my-2  text-xl rounded-full font-semibold"
+              />
+
+              <button className="bg-[#1D9Bf0] my-4  py-2 border-none rounded-full text-lg text-white font-bold  cursor-pointer ">
+                {islogin ? "Login" : "Create Account"}
+              </button>
+              <h1>
+                {islogin ? "Do not have account?" : "Already have an account ?"}
+                <span
+                  onClick={loginsignuphandler}
+                  className="font-bold text-blue-600 cursor-pointer"
+                >
+                  {islogin ? "Signup" : "login"}
+                </span>
+              </h1>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
--- frontend/src/component/Profile.js
+++ frontend/src/component/Profile.js
@@ -1,23 +1,50 @@
 import React from "react";
 import { IoMdArrowBack } from "react-icons/io";
+import Avatar from "react-avatar";
+import { Link } from "react-router-dom";
 const Profile = () => {
   return (
-    <div className="w-[50%]">
-      <div>
-        <div className=" flex items-center py-2">
-          <div className="p-2 rounded-full hover:bg-gray-100 hover:cursor-pointer">
-            <IoMdArrowBack size={"24px"} />
-          </div>
-          <div className="ml-2">
-            {" "}
-            <h1 className="dont-bold text-lg ">Mayank</h1>
-            <p className="text-gray-500 text-sm"> 10 psot</p>
-          </div>
+    <div className="w-[50%] border-l border-r border-gray-200">
+      <div className=" flex items-center py-2">
+        <Link
+          to="/"
+          className="p-2 rounded-full hover:bg-gray-100 hover:cursor-pointer"
+        >
+          <IoMdArrowBack size={"24px"} />
+        </Link>
+        <div className="ml-2">
+          {" "}
+          <h1 className="dont-bold text-lg ">Mayank</h1>
+          <p className="text-gray-500 text-sm"> 10 post</p>
         </div>
-        <img
-          src="https://media.licdn.com/dms/image/D4D16AQGrnpD2edxZPA/profile-displaybackgroundimage-shrink_350_1400/0/1706947033005?e=1728518400&v=beta&t=AAHJoSm5PeKufIXLxqcqbmxhIYtZj-3_zf0vkOyyWyk"
-          alt=""
+      </div>
+      <img
+        src="https://media.licdn.com/dms/image/D4D16AQGrnpD2edxZPA/profile-displaybackgroundimage-shrink_350_1400/0/1706947033005?e=1728518400&v=beta&t=AAHJoSm5PeKufIXLxqcqbmxhIYtZj-3_zf0vkOyyWyk"
+        alt=""
+      />
+
+      <div className="absolute top-52 ml-2 rounded-full border-4 border-white">
+        <Avatar
+          src="https://avatars.githubusercontent.com/u/134360258?v=4"
+          size={"110px"}
+          round={true}
         />
+      </div>
+      <div className="text-right m-4">
+        <button className="px-4  py-1 hover:bg-gray-200 rounded-full border-gray-400">
+          Edit Profile
+        </button>
+      </div>
+      <div className="ml-4 mt-4">
+        <h1 className="font-bold text-xl mt-4 ">Mayank</h1>
+        <p>@patelmernstack</p>
+      </div>
+      <div className="ml-4">
+        <p>
+          BCA 3rd year student, working on strengthening the roots and
+          developing real-life projects. - Join me on this journey and progress
+          ahead together.
+        </p>
       </div>
     </div>
   );
