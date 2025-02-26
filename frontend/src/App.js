e0be602a15ac33e3649ce91f6e761ffde7698c33
= <mayankrathore9897@gmail.com>
2025-02-26 22:45:02 +0530

    working on frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import Body from "./component/Body";
 import Toaster from "react-hot-toast";
+import { useState } from "react";
 function App() {
+  const[state,setstate]=useSate(0)
   return (
     <>
       <Body />
-      <Toaster />
+      <Toaster/>
     </>
   );
 }
